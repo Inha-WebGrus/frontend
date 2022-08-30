6076a4b839409d941f66e5d323c05a466233822a
0622kimny <100821679+0622kimny@users.noreply.github.com>
2022-08-30 21:39:20 +0900

    Add files via upload

---
+++ main/card.js
@@ -0,0 +1,19 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import React from 'react';
+import './App.css'
+
+function BasicExample() {
+  return (
+    <Card className='Card'>
+      <Card.Img variant="top" className='CardImg'
+        src="/cupcakes-gb1928ac49_640.jpg" />
+      <Card.Body style={{textAlign: 'left'}}>
+        <Card.Title className='CardTitle'>인하대 베이커리 맛집</Card.Title>
+        <Card.Text className='CardText'>디저트</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default BasicExample;
